46dcaaec30eda126bcbad5baa0231ca557aab3b8
VaganovAnil <70805522+VaganovAnil@users.noreply.github.com>
2020-10-25 23:59:28 +0500

    feat: add basic task layout

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+    if (!matrix) return [];
+    let arr = [];
+    let sort = 1;
+    for (let i = 0; i < matrix.length; i++) {
+        if (sort === 1) {
+            for (let j = 0; j < matrix[i].length; j++) {
+                arr.push(matrix[i][j]);
+            }
+            sort = 0;
+            continue;
+        }
+        if (sort === 0) {
+            for (let j = matrix[i].length - 1; j >= 0; j--) {
+                arr.push(matrix[i][j]);
+            }
+            sort = 1;
+            continue;
+        }
+    }
+    return arr;
 }
